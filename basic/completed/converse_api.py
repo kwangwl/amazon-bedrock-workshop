591b0b59f4347437c6661d5dccca3650821e4542
kwolell <kwolell@gmailc.om>
2024-07-25 23:03:48 +0900

    converse_api

---
+++ basic/completed/converse_api.py
@@ -0,0 +1,30 @@
+import boto3
+import json
+
+session = boto3.Session()
+bedrock = session.client(service_name='bedrock-runtime')
+
+message = {
+    "role": "user",
+    "content": [
+        {"text": "대한민국에 섬은 총 몇개인가요?"}
+    ],
+}
+
+response = bedrock.converse(
+    modelId="anthropic.claude-3-sonnet-20240229-v1:0",
+    messages=[message],
+    inferenceConfig={
+        "maxTokens": 2000,
+        "temperature": 0
+    },
+)
+
+print("\n---- output ----")
+print(json.dumps(response["output"], indent=4, ensure_ascii=False))
+
+print("\n---- usage ----")
+print(json.dumps(response['usage'], indent=4))
+
+print("\n---- metrics ----")
+print(json.dumps(response['metrics'], indent=4))
