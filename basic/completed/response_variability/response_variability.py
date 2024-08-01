afc044da5a0d5545dfdbf93b34fbea1cca724ff7
kwolell <kwolell@gmailc.om>
2024-08-01 14:20:07 +0900

    streamlit

--- basic/completed/response_variability/response_variability.py
+++ basic/completed/response_variability/response_variability.py
@@ -2,7 +2,7 @@ import boto3
 import sys
 
 
-def get_text_response(input_content, temperature):
+def get_text_response(input_content, model_id, temperature):
     session = boto3.Session()
     bedrock = session.client(service_name='bedrock-runtime')
 
@@ -12,7 +12,7 @@ def get_text_response(input_content, temperature):
     }
 
     response = bedrock.converse(
-        modelId="anthropic.claude-3-sonnet-20240229-v1:0",
+        modelId=model_id,
         messages=[message],
         inferenceConfig={
             "maxTokens": 2000,
@@ -25,6 +25,7 @@ def get_text_response(input_content, temperature):
     return response['output']['message']['content'][0]['text']
 
 
+prompt = "생성형 AI에 대해 한줄로 설명해줘"
 for i in range(3):
-    response = get_text_response(sys.argv[1], float(sys.argv[2]))
+    response = get_text_response(prompt, sys.argv[1], float(sys.argv[2]))
     print(response, end='\n\n')
