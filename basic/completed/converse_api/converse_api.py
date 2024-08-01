b41b52fab2ee0b018464bcbf59d6110d5a17e6a0
kwolell <kwolell@gmailc.om>
2024-08-01 13:49:53 +0900

    converse api

--- basic/completed/converse_api/converse_api.py
+++ basic/completed/converse_api/converse_api.py
@@ -1,31 +1,40 @@
 import boto3
 import json
 
-session = boto3.Session()
-bedrock = session.client(service_name='bedrock-runtime')
+def chunk_handler(chunk):
+    print(chunk, end='')
 
-model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
-message = {
-    "role": "user",
-    "content": [
-        {"text": "대한민국에 섬은 총 몇개인가요?"}
-    ],
-}
+def get_streaming_response(prompt, streaming_callback):
+    session = boto3.Session()
+    bedrock = session.client(service_name='bedrock-runtime')
 
-response = bedrock.converse(
-    modelId=model_id,
-    messages=[message],
-    inferenceConfig={
-        "maxTokens": 2000,
-        "temperature": 0
-    },
-)
+    message = {
+        "role": "user",
+        "content": [{"text": prompt}]
+    }
 
-print("\n---- output ----")
-print(json.dumps(response["output"], indent=4, ensure_ascii=False))
+    response = bedrock.converse_stream(
+        modelId="anthropic.claude-3-sonnet-20240229-v1:0",
+        messages=[message],
+        inferenceConfig={
+            "maxTokens": 2000,
+            "temperature": 0.0
+        }
+    )
 
-print("\n---- usage ----")
-print(json.dumps(response['usage'], indent=4))
+    print("---- Streaming Response ----")
+    stream = response.get('stream')
+    for event in stream:
+        if "contentBlockDelta" in event:
+            streaming_callback(event['contentBlockDelta']['delta']['text'])
 
-print("\n---- metrics ----")
-print(json.dumps(response['metrics'], indent=4))
+        if "metadata" in event:
+            # Assuming the response contains 'usage' and 'metrics' fields
+            print("\n\n---- usage ----")
+            print(json.dumps(event['metadata']['usage'], indent=4))
+
+            print("\n---- metrics ----")
+            print(json.dumps(event['metadata']['metrics'], indent=4))
+
+prompt = "대한민국에 섬은 총 몇개인가요?"
+get_streaming_response(prompt, chunk_handler)
