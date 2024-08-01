1db5be4a1a7597551a9d073918e91295270a0276
kwolell <kwolell@gmailc.om>
2024-08-01 13:57:47 +0900

    converse api

--- basic/completed/converse_api/converse_api.py
+++ basic/completed/converse_api/converse_api.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import sys
 
 def chunk_handler(chunk):
     print(chunk, end='')
@@ -29,12 +30,9 @@ def get_streaming_response(prompt, streaming_callback):
             streaming_callback(event['contentBlockDelta']['delta']['text'])
 
         if "metadata" in event:
-            # Assuming the response contains 'usage' and 'metrics' fields
             print("\n\n---- usage ----")
             print(json.dumps(event['metadata']['usage'], indent=4))
-
             print("\n---- metrics ----")
             print(json.dumps(event['metadata']['metrics'], indent=4))
 
-prompt = "대한민국에 섬은 총 몇개인가요?"
-get_streaming_response(prompt, chunk_handler)
+get_streaming_response(sys.argv[1], chunk_handler)
