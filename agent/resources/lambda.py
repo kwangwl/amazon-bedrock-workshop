99a81ba0369d55f756b5cff698101a831dba92fd
kwolell <kwolell@gmailc.om>
2024-08-10 23:42:12 +0900

    add

--- agent/resources/lambda.py
+++ agent/resources/lambda.py
@@ -26,25 +26,32 @@ def get_stock_chart(ticker):
 
 
 def get_stock_balance(ticker):
-    # yfinance를 사용하여 주식 티커에 해당하는 회사 정보 가져오기
     company = yf.Ticker(ticker)
 
-    # 대차대조표 가져오기
-    balance_sheet = company.balance_sheet
+    # 재무제표 가져와서 최근 3년간의 데이터 선택
+    balance = company.balance_sheet
+    if balance.shape[1] >= 3:
+        balance = balance.iloc[:, :3]
+    balance = balance.dropna(how="any")
 
-    # 대차대조표에서 최근 3년간의 데이터 선택
-    if balance_sheet.shape[1] >= 3:
-        balance_sheet = balance_sheet.iloc[:, :3]  # 최근 3년간의 데이터만 선택
+    prompt = "이 데이터는 주식의 재무제표 정보를 나타냅니다:\n"
+    for col in balance.columns:
+        date = col.strftime('%Y-%m-%d')
+        prompt += f"\n{date}:\n"
+        for item, value in balance[col].items():
+            prompt += f"{item}: {value}\n"
+    return prompt
 
-    # 결측값이 있는 행 제거
-    balance_sheet = balance_sheet.dropna(how="any")
 
-    # output 정제
-    balance_sheet_dict = {}
-    for date, value in balance_sheet.items():
-        balance_sheet_dict.update({date.strftime('%Y-%m-%d'): {key: int(item) for key, item in value.items()}})
+def get_recommendations(ticker):
+    stock = yf.Ticker(ticker)
+    recommendations = stock.recommendations
 
-    return balance_sheet_dict
+    prompt = "주식의 시간대별 애널리스트 추천 정도는 다음과 같습니다.:\n"
+    for index, row in recommendations.iterrows():
+        prompt += (f"{row['period']}에는 강력 매수 {row['strongBuy']}건, 매수 {row['buy']}건, 보유 {row['hold']}건,"
+                   f" 매도 {row['sell']}건, 강력 매도 {row['strongSell']} 입니다.\n")
+    return prompt
 
 
 def lambda_handler(event, context):
@@ -66,6 +73,12 @@ def lambda_handler(event, context):
         output = get_stock_balance(ticker)
         responseBody = {'TEXT': {'body': json.dumps(output)}}
 
+    elif function == 'get_recommendations':
+        # 예시로 사용할 주식 티커
+        ticker = get_named_parameter(event, "ticker")
+        output = get_recommendations(ticker)
+        responseBody = {'TEXT': {'body': json.dumps(output)}}
+
     else:
         responseBody = {'TEXT': {'body': 'Invalid function'}}
 
@@ -84,11 +97,9 @@ def lambda_handler(event, context):
 
 
 if __name__ == "__main__":
-    msft = yf.Ticker("MSFT")
-
     # 설정
     event = {
-      "function": "get_stock_chart",
+      "function": "get_stock_balance",
       "parameters": [
         {
           "name": "ticker",
